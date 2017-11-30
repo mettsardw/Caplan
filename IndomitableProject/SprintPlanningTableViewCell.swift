46c84cd6b91a4478826774f8e1eb8ef931f3f258
maritnus <martinus_36@yahoo.com>
2017-11-30 10:29:50 +0700

    task UI

---
+++ IndomitableProject/SprintPlanningTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SprintPlanningTableViewCell.swift
+//  IndomitableProject
+//
+//  Created by Martinus on 30/11/17.
+//  Copyright © 2017 RagingWind. All rights reserved.
+//
+
+import UIKit
+
+class SprintPlanningTableViewCell: UITableViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var detailSprintPlanningButton: UIButton!
+}
