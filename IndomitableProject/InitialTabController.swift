85b55f61652f160508f9be1a590a5b65396d7126
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2017-11-30 17:20:48 +0700

    tes core data

--- IndomitableProject/InitialTabController.swift
+++ IndomitableProject/InitialTabController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class InitialTabController: UITabBarController {
     
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         //print("first")
+        let container = appDelegate.persistentContainer.viewContext
+        let taskEntity = NSEntityDescription.entity(forEntityName: "TaskCore", in: container)
+        let eventEntity = NSEntityDescription.entity(forEntityName: "EventCore", in: container)
+        let newTask = NSManagedObject(entity: taskEntity!, insertInto: container)
+        let newEvent = NSManagedObject(entity: eventEntity!, insertInto: container)
+        newTask.setValue("Requirement", forKey: "name")
         
-       manageProject()
+        newTask.mutableSetValue(forKey: "event").add(newEvent)
+        //newTask.objectIDs(forRelationshipNamed: "event
+        
+        manageProject()
+        let taskFetch = NSFetchRequest<NSManagedObject>(entityName: "TaskCore")
+        do{
+            let tasks: [NSManagedObject] = try container.fetch(taskFetch)
+            for task in tasks {
+                print("name: \(task.value(forKey: "name") as! String)")
+                //print(task.objectIDs(forRelationshipNamed: "event").count)
+                
+            }
+        }catch let error as NSError {
+            print("error")
+        }
     }
 }
 
