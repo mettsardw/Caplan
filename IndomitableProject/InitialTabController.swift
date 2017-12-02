7911b4eceacd5b6e195cc24ec1d4751e3ccf7500
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2017-12-02 20:51:46 +0700

    Input data success
    yass!!

--- IndomitableProject/InitialTabController.swift
+++ IndomitableProject/InitialTabController.swift
@@ -18,53 +18,56 @@ class InitialTabController: UITabBarController {
         let container = appDelegate.persistentContainer.viewContext
         let taskEntity = NSEntityDescription.entity(forEntityName: "TaskCore", in: container)
         let eventEntity = NSEntityDescription.entity(forEntityName: "EventCore", in: container)
+        let timeEntity = NSEntityDescription.entity(forEntityName: "TimeCore", in: container)
         //let newTask = NSManagedObject(entity: taskEntity!, insertInto: container)
         //let newEvent = NSManagedObject(entity: eventEntity!, insertInto: container)
-       
-        let newTask = TaskCore(entity: taskEntity!, insertInto: container)
-        let newEvent = EventCore(entity: eventEntity!, insertInto: container)
-        let newEvent1 = EventCore(entity: eventEntity!, insertInto: container)
-        let newEvent2 = EventCore(entity: eventEntity!, insertInto: container)
-        newTask.setValue("Requirement", forKey: "name")
-//        newTask.setValue("Design", forKey: "name")
-//        newTask.setValue("Coding", forKey: "name")
-//        newTask.setValue("Testing", forKey: "name")
+//        let newTime = TimeCore(entity: timeEntity!, insertInto: container)
+//        let newTask = TaskCore(entity: taskEntity!, insertInto: container)
+//        let newEvent = EventCore(entity: eventEntity!, insertInto: container)
+//        let newEvent1 = EventCore(entity: eventEntity!, insertInto: container)
+//        let newEvent2 = EventCore(entity: eventEntity!, insertInto: container)
 //        newTask.setValue("Requirement", forKey: "name")
-//        newTask.setValue("Requirement", forKey: "name")
-        newEvent.setValue("Gather Data", forKey: "type")
-        newEvent1.setValue("Market Data", forKey: "type")
-        newEvent2.setValue("requirement analyst", forKey: "type")
-        //tipe data Int Masih bug
-        newEvent.setValue(6, forKey: "memberCount")
-        newEvent1.setValue(Int("6"), forKey: "memberCount")
-        newEvent2.setValue(Int("6"), forKey: "memberCount")
-        newTask.addToEvent(newEvent)
-        newTask.addToEvent(newEvent2)
-        newTask.addToEvent(newEvent1)
+//        newTask.setValue(15, forKey: "memberCount")
+//        newTask.setValue(10, forKey: "point")
+//        newTask.setValue(1, forKey: "priority")
+//        newTask.setValue("lorem ipsum dolor sit amet", forKey: "notes")
+//        newTime.setValue(3, forKey: "duration")
+//        newTime.setValue("day", forKey: "unit")
+//        newTask.time = newTime
+//        //print(newTask.time as Any)
+////        newTask.setValue("Design", forKey: "name")
+////        newTask.setValue("Coding", forKey: "name")
+////        newTask.setValue("Testing", forKey: "name")
+////        newTask.setValue("Requirement", forKey: "name")
+//        newEvent.setValue("Requirement Gathering", forKey: "type")
+//        newEvent1.setValue("Market Analysis", forKey: "type")
+//        newEvent2.setValue("Requirement Validation", forKey: "type")
+//        //tipe data Int Masih bug kalo angkany sama.. wtf menn..
+//        newEvent.setValue(6, forKey: "memberCount")
+//        newEvent1.setValue(Int("9"), forKey: "memberCount")
+//        newEvent2.setValue(Int("7"), forKey: "memberCount")
+//        newTime.setValue(1, forKey: "duration")
+//        newTime.setValue("day", forKey: "unit")
+//        newEvent.time = newTime
+//        newEvent1.time = newTime
+//        newEvent2.time = newTime
+//
+//
+//        newTask.addToEvent(newEvent)
+//        newTask.addToEvent(newEvent2)
+//        newTask.addToEvent(newEvent1)
         
-        //newTask.mutableSetValue(forKey: "event").add(newEvent)
-        //newTask.objectIDs(forRelationshipNamed: "event
+//        do {
+//            try container.save()
+//            print("save success")
+//        } catch _ as NSError {
+//            print("save error")
+//        }
+        
+        //container.delete(newTask)
     
         manageProject()
-        let taskFetch = NSFetchRequest<NSManagedObject>(entityName: "TaskCore")
-        do{
-            let tasks: [NSManagedObject] = try container.fetch(taskFetch)
-            for task in tasks {
-                print("name: \(task.value(forKey: "name") as! String)")
-                //get NSSet to String
-                guard let taskEvent = newTask.event?.value(forKeyPath: "memberCount") else {print("no event");return}
-                print(taskEvent)
-                //
-                let taskEventDataValue = getStringFromNSSet(data: taskEvent)
-                print(taskEventDataValue[0])
-//                print(taskEventDataValue[1])
-//                print(taskEventDataValue[2])
-//               
-                
-            }
-        }catch _ as NSError {
-            print("error")
-        }
+
     }
 }
 
@@ -75,10 +78,45 @@ func manageProject(){
     //initialize project, tasks.
     let plan: Plan = Plan(name: "Planning")
     //var plan2: Plan = Plan(name: "Sprinting")
-    plan.addTask(task: Task(name: .Requirement, memberCount: 1, time: Time(duration: 10, unit: .days)))
-    plan.addTask(task: Task(name: .Design, memberCount: 1, time: Time(duration: 5, unit: .days)))
-    plan.addTask(task: Task(name: .Coding, memberCount: 1, time: Time(duration: 7, unit: .days)))
+    //plan.addTask(task: Task(name: .Requirement, memberCount: 1, time: Time(duration: 10, unit: .days)))
+    //plan.addTask(task: Task(name: .Design, memberCount: 1, time: Time(duration: 5, unit: .days)))
+    //plan.addTask(task: Task(name: .Coding, memberCount: 1, time: Time(duration: 7, unit: .days)))
   
+    let taskFetch = NSFetchRequest<NSManagedObject>(entityName: "TaskCore")
+    do{
+        let tasks: [TaskCore] = try container.fetch(taskFetch) as! [TaskCore]
+        for task in tasks {
+            print("name: \(task.value(forKey: "name") as! String)")
+            print("member Count: \(task.value(forKey: "memberCount")!)")
+            print("point: \(task.value(forKey: "point")!)")
+            guard let taskTimeUnit = task.time?.unit else{print("no unit");return}
+            print("time: \(String(describing: task.time!.duration)) \(taskTimeUnit)")
+            
+            //delete data di Core data
+            //                container.delete(task)
+            //                do {
+            //                    try container.save()
+            //                    print("save success")
+            //                } catch _ as NSError {
+            //                    print("save error")
+            //                }
+            
+            //print("task value:\n\(String(describing: task.value(forKey: "event")!))")
+            //get NSSet to String
+            //                guard let taskEvent = task.event?.value(forKeyPath: "memberCount") else {print("no event");return}
+            //                print(taskEvent)
+            //
+            //let taskEventDataValue = getStringFromNSSet(data: taskEvent)
+            //print(taskEventDataValue[0])
+            //                print(taskEventDataValue[1])
+            //                print(taskEventDataValue[2])
+            //
+            
+        }
+    }catch _ as NSError {
+        print("error")
+    }
+    
     project.schedule.plans.append(plan)
 }
 
--- IndomitableProject/UIAddTask/TaskPickerView.swift
+++ IndomitableProject/UIAddTask/TaskPickerView.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class TaskPickerView: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBOutlet weak var alert: UILabel!
     @IBAction func showActDetail(_ sender: UIButton) {
-        performSegue(withIdentifier: "taskDetail", sender: nil)
+        if (numberMember.text?.isEmpty)! || numberMember.text == "0" {
+            alert.text = "set your member first!"
+        
+        }else{
+            alert.text = ""
+            performSegue(withIdentifier: "taskDetail", sender: nil)
+        }
     }
     @IBOutlet weak var dataPicker: UIPickerView!
     var taskData: [String] = ["Requirement","Design","Coding","Testing","Review"]
@@ -74,6 +81,7 @@ class TaskPickerView: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         dataPicker.dataSource = self
         dataPicker.delegate = self
+        alert.text = ""
         // Do any additional setup after loading the view.
     }
 
